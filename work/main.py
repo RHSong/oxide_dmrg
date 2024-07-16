9bce53c421f83c8158dd61a0e387e71993556960
Junjie Yang <yangjunjie0320@gmail.com>
2024-07-16 14:41:51 -0700

    update

--- work/main.py
+++ work/main.py
@@ -15,36 +15,42 @@ thrds = [1e-10] * 12
 n_threads = os.environ.get("OMP_NUM_THREADS", "1")
 
 def main(dista):
-	print("\n\nRunning calculation for dista = %12.8f" % dista)
-	assert dista > 0.0
+    print("\n\n=====================================================")
+    print("Running calculation for dista = %12.8f" % dista)
+    print("bond_dims = %s" % bond_dims)
+    print("noises = %s" % noises)
+    print("thrds = %s" % thrds)
+    print("n_threads = %s" % n_threads)
 
-	mol = gto.Mole()
-	mol.atom = [['O',(0.0, 0.0, 0.0)], ['O',(0.0, 0.0, dista)],]
-	mol.spin = 2
-	mol.basis = 'cc-pvdz'
-	mol.symmetry = 'C2v'
-	mol.build()
+    assert dista > 0.0
 
-	# RHF case (for spin-adapted / non-spin-adapted DMRG)
-	mf = scf.RHF(mol).run()
-	ncas, n_elec, spin, ecore, h1e, g2e, orb_sym = itg.get_rhf_integrals(
-		mf, ncore=0, ncas=None, g2e_symm=8
-		)
-	print("ncas = %s, n_elec = %s, spin = %s, ecore = %s" % (ncas, n_elec, spin, ecore))
-	print("orb_sym = %s" % orb_sym)
+    mol = gto.Mole()
+    mol.atom = [['O',(0.0, 0.0, 0.0)], ['O',(0.0, 0.0, dista)],]
+    mol.spin = 2
+    mol.basis = 'cc-pvdz'
+    mol.symmetry = 'C2v'
+    mol.build()
 
-	driver = DMRGDriver(scratch=tmpdir, symm_type=SymmetryTypes.SU2, n_threads=int(n_threads))
-	driver.initialize_system(n_sites=ncas, n_elec=n_elec, spin=spin, orb_sym=orb_sym)
+    # RHF case (for spin-adapted / non-spin-adapted DMRG)
+    mf = scf.RHF(mol).run()
+    ncas, n_elec, spin, ecore, h1e, g2e, orb_sym = itg.get_rhf_integrals(
+        mf, ncore=0, ncas=None, g2e_symm=8
+        )
+    print("ncas = %s, n_elec = %s, spin = %s, ecore = %s" % (ncas, n_elec, spin, ecore))
+    print("orb_sym = %s" % orb_sym)
 
-	mpo = driver.get_qc_mpo(h1e=h1e, g2e=g2e, ecore=ecore, iprint=1)
-	ket = driver.get_random_mps(tag="GS", bond_dim=250, nroots=1)
-	energy = driver.dmrg(
-		mpo, ket, n_sweeps=20, bond_dims=bond_dims, 
-		noises=noises, thrds=thrds, iprint=1
-		)
-	
-	print('Final result: dista = %12.8f, energy = %12.8f' % (dista, energy))
+    driver = DMRGDriver(scratch=tmpdir, symm_type=SymmetryTypes.SU2, n_threads=int(n_threads))
+    driver.initialize_system(n_sites=ncas, n_elec=n_elec, spin=spin, orb_sym=orb_sym)
+
+    mpo = driver.get_qc_mpo(h1e=h1e, g2e=g2e, ecore=ecore, iprint=1)
+    ket = driver.get_random_mps(tag="GS", bond_dim=250, nroots=1)
+    energy = driver.dmrg(
+        mpo, ket, n_sweeps=20, bond_dims=bond_dims, 
+        noises=noises, thrds=thrds, iprint=1
+    )
+
+    print('Final result: dista = %12.8f, energy = %12.8f' % (dista, energy))
 
 if __name__ == "__main__":
-	main(1.2)
-	main(3.0)
+    main(1.2)
+    main(3.0)
